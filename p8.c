468f1e5669aa22f64d4bd0fdedd26c3ef8ff209f
bogdanoancea <bogdan.oancea@gmail.com>
2018-11-06 13:41:39 +0200

    small changes

--- p8.c
+++ p8.c
@@ -6,6 +6,7 @@ int suma (int x, int y) {
 	s = x + y;
 	return s;
 }
+
 int main() {
 	int a, b, c;
 	printf("Introduceti a: "); 
@@ -14,5 +15,5 @@ int main() {
 	scanf("%d",&b);
 	c = suma(a, b);
 	printf("Suma este: %d\n", c);
-
+	return 0;
 }
