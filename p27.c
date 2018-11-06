3fb4b88b9af7eea341dc18edc38eb707373b9fb7
bogdanoancea <bogdan.oancea@gmail.com>
2018-11-06 14:44:05 +0200

    small changes

--- p27.c
+++ p27.c
@@ -1,37 +1,36 @@
 #include<stdio.h>
 
 int produs1(int n) { //cu instructiunea FOR
-	int i, P = 1;
+	int i, p = 1;
 	for(i = 1; i <= n; i++)
-		P = P * i;
-	return P;
+		p = p * i;
+	return p;
 }
 
 int produs2(int n)	{ //cu instructiunea WHILE
-	int P = 1, i = 1; 
+	int p = 1, i = 1; 
 	while(i <= n) {
-		P = P * i;
+		p = p * i;
 		i++;
 	}
-	return P;
+	return p;
 }
 
 int produs3(int n) { //cu instructiunea DO WHILE
-{
-	int P = 1, i = 1; 
+	int p = 1, i = 1; 
 	do {
-		P = P * i;
+		p = p * i;
 		i++;
 	} while (i <= n);
-	return P;
+	return p;
 }
 
 int main() {
 	int n; 
 	printf("Introduceti n: ");
 	scanf("%d", &n);
-	printf("\n\tProdusul calculat cu FOR are valoarea: %d.\n" , produs1(n));
-	printf("\n\tProdusul calculat cu WHILE are valoarea: %d.\n" , produs2(n));
-	printf("\n\tProdusul calculat cu DO WHILE are valoarea: %d.\n" , produs3(n));
+	printf("\nProdusul calculat cu FOR are valoarea: %d.\n" , produs1(n));
+	printf("\nProdusul calculat cu WHILE are valoarea: %d.\n" , produs2(n));
+	printf("\nProdusul calculat cu DO WHILE are valoarea: %d.\n" , produs3(n));
 	return 0;
 }
