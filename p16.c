bde98b191ab1e3ac9c4405ce4f1c52560dc1678a
bogdanoancea <bogdan.oancea@gmail.com>
2018-11-06 13:57:50 +0200

    small changes

--- p16.c
+++ p16.c
@@ -3,9 +3,15 @@
 int main() {
 	float a, b, c;
 	printf("Dati lungimile laturilor:\n");
-	scanf("%f %f %f", &a, &b, &c);
+	printf("a=");
+	scanf("%f", &a);
+	printf("b=");
+	scanf("%f", &b);
+	printf("c=");
+	scanf("%f", &c);
+	
  
-	(a >= 0 && b >= 0 && c >= 0 && a < b + c && b < a + c && c < a + b) ? printf("\t %4.2f, %4.2f si %4.2f formeaza triunghi.", a, b, c):printf ("\t Nu formeaza triunghi.");
+	(a >= 0 && b >= 0 && c >= 0 && a < b + c && b < a + c && c < a + b) ? printf("%4.2f, %4.2f si %4.2f formeaza un triunghi.", a, b, c) : printf ("%4.2f, %4.2f si %4.2f nu formeaza un triunghi.",a ,b, c);
 	return 0;
 }
 
