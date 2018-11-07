c0e39c4a3a4c71763fe8ae16d47bb14eb8753f6c
bogdanoancea <bogdan.oancea@gmail.com>
2018-11-07 15:57:02 +0200

    small changes

---
+++ p60.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+
+int main () {
+	char linie[128], *cuv; // adresa cuvant in linie
+	char *sep = ".,;\t\n "; // sir de caractere separator
+	puts("Introduceti o linie de text:");
+	gets(linie); // citire linie
+	cuv = strtok(linie, sep); // primul cuvant din linie
+	puts("Cuvintele introduse sunt:");
+	while ( cuv != NULL ) {
+		puts(cuv); // scrie cuvant
+		cuv = strtok(0, sep); // urmatorul cuvant din linie
+	}
+	return 0;
+}
