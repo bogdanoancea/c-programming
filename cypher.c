d355451cbb0cfa19d429a50598a5f901b8469633
Bogdan Oancea <bogdan.oancea@gmail.com>
2019-01-04 20:49:02 +0200

    new functions added

--- cypher.c
+++ cypher.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 
 
@@ -10,10 +11,13 @@ char* get_key(int l, char* param[]);
 char* get_mode(int l, char* param[]); 
 char* get_alg(int l, char* param[]); 
 void encrytion(FILE* pf_in, FILE* pf_out, char* alg, char* key);
-//void decrytion(FILE* pf_in, FILE* pf_out, char* alg, char* key);
+void decrytion(FILE* pf_in, FILE* pf_out, char* alg, char* key);
 void en_caesar(FILE* pf_in, FILE* pf_out, char* key);
+void dec_caesar(FILE* pf_in, FILE* pf_out, char* key);
 void en_vigenere(FILE* pf_in, FILE* pf_out, char* key);
+void dec_vigenere(FILE* pf_in, FILE* pf_out, char* key);
 void en_xor(FILE* pf_in, FILE* pf_out, char* key);
+void dec_xor(FILE* pf_in, FILE* pf_out, char* key);
 
 void preprocesare_caesar(char* buf);
 
@@ -64,7 +68,7 @@ int main(int argc, char* argv[] ) {
     if(strcmp(mode, "enc") == 0) {
         encrytion(pf_in, pf_out, alg, key);
     } else {
-        //decrytion(pf_in, pf_out, alg, key);
+        decrytion(pf_in, pf_out, alg, key);
     }
 
     fclose(pf_in);
@@ -132,6 +136,28 @@ void encrytion(FILE* pf_in, FILE* pf_out, char* alg, char* key ) {
             en_xor(pf_in, pf_out, key);
 }
 
+void decrytion(FILE* pf_in, FILE* pf_out, char* alg, char* key ) {
+    if(strcmp(alg, "caesar") == 0)
+        dec_caesar(pf_in, pf_out, key);
+    else 
+        if(strcmp(alg, "vigenere") == 0)
+            dec_vigenere(pf_in, pf_out, key);
+        else
+            dec_xor(pf_in, pf_out, key);
+}
+
+void preprocesare_caesar(char* buf) {
+    int i;
+    for (i = 0; i < strlen(buf); i++) {
+        if(buf[i] >= 'A' && buf[i] <= 'Z')
+            buf[i]+=32;
+    }
+    for (i = 0; i < strlen(buf); i++) {
+        if(buf[i] < 'a' || buf[i] > 'z')
+            buf[i] ='q';
+    }
+}
+
 void en_caesar(FILE* pf_in, FILE* pf_out, char* key) {
  
     char buf[1024];
@@ -141,12 +167,14 @@ void en_caesar(FILE* pf_in, FILE* pf_out, char* key) {
     key_no = atoi(key);
    
     while( fgets(buf, 1024, pf_in) != NULL ) {
-  
+        buf[strcspn(buf, "\r\n")] = 0;
         preprocesare_caesar(buf);
         for(i = 0; i < strlen(buf); i++) {
-            buf[i] += key_no;
-            if(buf[i] >= 'z')
-                buf[i] = buf[i] - 'z' + 'a'- 1;
+            if(isalpha(buf[i])) {
+                buf[i] += key_no;
+                if(buf[i] >= 'z')
+                    buf[i] = buf[i] - 'z' + 'a' - 1;
+            }
         }
         puts(buf);
         strcat(buf, "\n");
@@ -155,19 +183,62 @@ void en_caesar(FILE* pf_in, FILE* pf_out, char* key) {
     }
 }
 
-void preprocesare_caesar(char* buf) {
+
+void dec_caesar(FILE* pf_in, FILE* pf_out, char* key) {
+ 
+    char buf[1024];
     int i;
-    for (i = 0; i < strlen(buf); i++) {
-        if(buf[i]>= 'A' && buf[i] <= 'Z')
-            buf[i]+=32;
-    }
-       for (i = 0; i < strlen(buf); i++) {
-        if(buf[i]< 'a' || buf[i] > 'z')
-            buf[i] ='q';
+    int key_no;
+
+    key_no = atoi(key);
+   
+    while( fgets(buf, 1024, pf_in) != NULL ) {
+        buf[strcspn(buf, "\r\n")] = 0;
+        for(i = 0; i < strlen(buf); i++) {
+            if(isalpha(buf[i])) {
+                buf[i] -= key_no;
+                if(buf[i] < 'a')
+                    buf[i] = buf[i] + 'z' - 'a' + 1;
+            }
+        }
+        puts(buf);
+        strcat(buf, "\n");
+        fputs(buf, pf_out);
+        
     }
 }
 
 
-
-void en_vigenere(FILE* pf_in, FILE* pf_out, char* key) {}
+void dec_vigenere(FILE* pf_in, FILE* pf_out, char* key) {}
 void en_xor(FILE* pf_in, FILE* pf_out, char* key){}
+void dec_xor(FILE* pf_in, FILE* pf_out, char* key){}
+
+
+void en_vigenere(FILE* pf_in, FILE* pf_out, char* key) {
+    
+    char buf[1024];
+    int i, j;
+    int keyLength = strlen(key);
+    
+    while( fgets(buf, 1024, pf_in) != NULL ) {
+        //buf[strcspn(buf, "\r\n")] = 0;
+        for (int i = 0, j = 0, n = strlen(buf); i < n; i++) {   
+            // if alphabetic proceed with ciphering, else return normal character
+            if (isalpha(buf[i])) {
+            
+                // calculate ASCII code for the key position (j), wrap around beginning of keyword when end of keyword is reached
+                char keyReferenceValue = (isupper(key[j % keyLength])) ? 'A' : 'a';
+            
+                // calculate ASCII code of the first letter of alphabet depending on upper- or lowercase
+                char referenceValue = (isupper(buf[i])) ? 'A' : 'a';
+            
+                // calculate cipher letter using formula ci = (pi + kj) % 26, then convert to right ASCII character number
+                buf[i] = ((buf[i] - referenceValue + (key[(j % keyLength)] - keyReferenceValue)) % 26) + referenceValue;
+                j++;
+            }
+        }    
+        puts(buf);
+        fputs(buf, pf_out);
+        
+    }
+}
